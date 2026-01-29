4050a438faf5186b2806af11b782d817e521e75f
Chanchal Mathur <chanchalmathur395@gmail.com>
2026-01-29 11:43:21 +0530

    Initial commit

---
+++ 29Jan/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/plain");
+    res.end("Welcome to the Node.js Tutorial");
+});
+server.listen(3000, ()=>{
+    console.log("Server is running on http://localhost:3000");
+});
